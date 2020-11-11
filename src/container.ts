c5f82548e3dec4e33035be92db4bad01a821d7c5
Matías Capuano <maticapuano97@gmail.com>
2020-11-11 11:00:15 -0600

    Added & implemented test servince.

--- src/container.ts
+++ src/container.ts
@@ -1,6 +1,7 @@
-import { createContainer } from "awilix";
+import { asClass, createContainer } from "awilix";
 import { scopePerRequest } from "awilix-express";
 import { Application } from "express";
+import { TestService } from "./services/test.service";
 
 export default (app: Application): void => {
   const container = createContainer({ injectionMode: "CLASSIC" });
@@ -8,6 +9,7 @@ export default (app: Application): void => {
   //Container register
   container.register({
     //Services
+    testService: asClass(TestService).scoped(),
   });
 
   app.use(scopePerRequest(container));
