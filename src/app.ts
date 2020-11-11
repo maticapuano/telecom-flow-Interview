76e184b41c20eb88f80379d295b701325876a43d
Matías Capuano <maticapuano97@gmail.com>
2020-11-11 10:54:02 -0600

    Implemented container register.

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,18 @@
+import { loadControllers } from "awilix-express";
 import express, { Application } from "express";
+import registerContainer from "./container";
 
 const app: Application = express();
 
 const ROUTE_PREFIX = process.env.ROUTE_PREFIX || "/v1";
 
-app.use(ROUTE_PREFIX, (req, res) => {
-  res.json({ work: true });
-});
+//Register containers
+registerContainer(app);
+
+//Load controllers
+app.use(
+  ROUTE_PREFIX,
+  loadControllers("controllers/*/*.controller.ts", { cwd: __dirname })
+);
 
 export { app };
