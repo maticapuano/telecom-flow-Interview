71c9e40405022b371ae08db68e4891697f712f88
Matías Capuano <maticapuano97@gmail.com>
2020-11-11 11:14:38 -0600

    Added dotenv.

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,12 @@
 import { app } from "./app";
+import dotenv from "dotenv";
+import path from "path";
 
 const PORT = process.env.PORT || 3000;
+
+//Configure variables env
+dotenv.config({
+  path: path.resolve(process.cwd(), ".env"),
+});
 
 app.listen(PORT, () => console.log(`Server ready on port ${PORT}`));
