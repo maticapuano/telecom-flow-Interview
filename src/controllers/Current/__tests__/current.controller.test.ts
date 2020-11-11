1844be32d21ad187cedc491547bd9a946e6278b5
Matías Capuano <maticapuano97@gmail.com>
2020-11-11 12:05:13 -0600

    Implemented current controller.

---
+++ src/controllers/Current/__tests__/current.controller.test.ts
@@ -0,0 +1,11 @@
+import request from "supertest";
+import { app } from "../../../app";
+
+const ROUTE_PREFIX = "/v1";
+
+it("Should return a 200", async () => {
+  const response = await request(app).get(`${ROUTE_PREFIX}/current`);
+
+  expect(response.status).toEqual(200);
+  expect(response.body.city).toBeDefined();
+});
